419f39449d2dfa5ac46c82032106340b1610ca3a
Esteban lau koo <Esteban lau koo>
2017-04-01 19:36:13 -0400

    this is just a little bit of work

--- liri.js
+++ liri.js
@@ -1,8 +1,9 @@
 var string = process.argv
-var operand = string[2]
+var operand = string.slice(2).join(' ')
 var twitter = require('twitter')
 var spotify = require('spotify')
 var omdb = require('request')
+console.log(operand)
 switch (operand) {
     case ('my-tweets'):
     twitterAPI()
@@ -24,31 +25,10 @@ function twitterAPI() {
 }
 function spotifyAPI() {
 //  This will show the following information about the song in your terminal/bash window
-    Artist(s)
-    The song's name
-    A preview link of the song from Spotify
-    The album that the song is from
 
-
-    if no song is provided then your program will default to
-    "The Sign" by Ace of Base
 }
 function omdbAPI() {
-    This will output the following information to your terminal/bash window:
-        * Title of the movie.
-    * Year the movie came out.
-    * IMDB Rating of the movie.
-    * Country where the movie was produced.
-    * Language of the movie.
-    * Plot of the movie.
-    * Actors in the movie.
-    * Rotten Tomatoes Rating.
-    * Rotten Tomatoes URL.
 
-
-        If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-    It's on Netflix!
 }
 function dixby() {
 
