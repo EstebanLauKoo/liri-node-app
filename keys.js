60d472dc5dbf90e418a030feb5adc993a4712065
Esteban lau koo <Esteban lau koo>
2017-04-02 01:58:53 -0400

    finished twitter npm

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
-
-exports.twitterKeys = {
+var twitter = require('twitter')
+exports.twitterKeys = new twitter({
     consumer_key: 'FJV2mGrypRW972iRTSXUCJsgk',
     consumer_secret: 'Q5hRJiJawpbTB3SUds8hs9iskwVIJ7AWG9OrHv8Il6lgaChwJr',
     access_token_key: '210083878-MhI6Wzybo8esif2M52pnsUvsk1kWx1lxNt237SNZ',
     access_token_secret: 'kPm6X8FhCN8t4Yi4MZwiPYXEopYL9BpGClFPCaz6JWyba',
-}
+})
--- liri.js
+++ liri.js
@@ -1,9 +1,21 @@
-var string = process.argv
-var operand = string.slice(2).join(' ')
-var twitter = require('twitter')
-var spotify = require('spotify')
-var omdb = require('request')
+var string = process.argv,
+    operand = string[2],
+    search = [],
+    spotify = require('spotify'),
+    omdb = require('request'),
+    keys = require('./keys.js'),
+    client = keys.twitterKeys,
+    params = {screen_name: searchQuery}
+
+    for (var i = 3; i < string.length; i++){
+        search.push(string[i])
+    }
+
+    var searchQuery = search.join(" ")
+
+console.log(searchQuery)
 console.log(operand)
+
 switch (operand) {
     case ('my-tweets'):
     twitterAPI()
@@ -18,8 +30,18 @@ switch (operand) {
     dixby()
     break
 }
+
 function twitterAPI() {
 // pass the require
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+                for (var i = 0; i < tweets.length; i++) {
+                    console.log([i+1]+ " " + tweets[i].text)
+                }
+            } else {
+                throw error
+            }
+    });
 // research how to make the api work
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
 }
